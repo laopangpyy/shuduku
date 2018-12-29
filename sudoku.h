67508ba5d64985b5dfdac5976d49c4a40017da46
laopangpyy <40331961+laopangpyy@users.noreply.github.com>
2018-12-29 11:55:52 +0800

    Add files via upload

--- sudoku.h
+++ sudoku.h
@@ -5,11 +5,17 @@ class ShuDu
 public:
 	int CreateShuDu(int n);
 	void SolveShuDu(char*);
-private:
-	char set_security[3][10][10] = {0};
+	void Solve(int r, int c);
+	void InputShuDu(int row, char *save);
+	int State_Create = 0;
+	int State_Solve = 1;
+
+//private:
+	int set_security[3][10][10] = {0};
 	char map[9][9] = {0};
 	bool IsSolve = false;
 	bool IsFirstShuDu = true;
-	void Solve(int r,int c);
-	void InputShuDu(int row, char *save);
+
 };
+
+int main(int argc, char**argv);
